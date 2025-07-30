1b664c7a1035262a7b6479359c85e060558b8e10
João Gabriel <jpcardoso0811@gmail.com>
2025-07-30 03:31:23 -0300

    Atualiza funcionalidade de busca por cidade com Enter, além de ajustes na imagem de fundo

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 let chave = "59b99d411091f76bd9d5436c74e014cb"
 
-
 function ColocarNaTela(dados) {
     console.log(dados)
 
@@ -8,27 +7,33 @@ function ColocarNaTela(dados) {
     document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
     document.querySelector(".descricao").innerHTML = dados.weather[0].description
     document.querySelector(".umidade").innerHTML = "Umidade: " + (dados.main.humidity) + "%"
-    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+    //document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
 }
 
 async function buscarCidade(cidade) {
-    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
-        cidade +
-        "&appid=" +
-        chave +
-        "&lang=pt_br" +
-        "&units=metric")
-        .then(resposta => resposta.json())
+    let dados = await fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+            cidade +
+            "&appid=" +
+            chave +
+            "&lang=pt_br" +
+            "&units=metric"
+    ).then((resposta) => resposta.json())
 
     ColocarNaTela(dados)
 }
 
-
-
-
 function cliqueiNoBotao() {
     let cidade = document.querySelector(".input-cidade").value
-
-   
     buscarCidade(cidade)
 }
+
+// Evento para disparar a busca ao apertar Enter no input
+document.addEventListener("DOMContentLoaded", () => {
+    const inputCidade = document.querySelector(".input-cidade")
+    inputCidade.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            cliqueiNoBotao()
+        }
+    })
+})
